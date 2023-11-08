b5cf5657ae5d972a0118fa519d57fe08f081735b
Shreya Agarwal <www.shreyaagarwal.com@gmail.com>
2023-11-08 18:10:19 +0530

    feat: toggle functionality in checkbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
 import Header from './components/Header';
 import CheckBoxes from './components/CheckBoxes';
 import Letters from './components/Letters';
+import { useState } from 'react';
+
+type CheckBoxState = {
+  checked: boolean;
+};
 
 function App() {
+  const [isCheckBoxChecked, setIsCheckBoxChecked] = useState<CheckBoxState[]>(
+    Array.from({ length: 7 }, () => ({ checked: false })),
+  );
+
+  const handleCheckBoxChange = (index: number) => {
+    const newCheckBoxState = [...isCheckBoxChecked];
+    newCheckBoxState[index].checked = !newCheckBoxState[index].checked;
+    setIsCheckBoxChecked(newCheckBoxState);
+  };
+
   return (
     <div className="h-screen flex flex-col">
       <Header />
       <div className="flex flex-row w-full">
-        <CheckBoxes />
+        <CheckBoxes
+          isCheckBoxChecked={isCheckBoxChecked}
+          onCheckBoxChange={handleCheckBoxChange}
+        />
         <div className="w-4/12 h-full flex flex-col px-10 mt-10">
           <h1 className="text-2xl font-semibold border-b-2 border-gray-300 py-4">
             Desired Result
--- src/components/CheckBoxes.tsx
+++ src/components/CheckBoxes.tsx
@@ -1,14 +1,21 @@
-const CheckBoxes = () => {
+const CheckBoxes = ({ isCheckBoxChecked, onCheckBoxChange }: any) => {
   return (
     <div className="w-4/12 px-10 mt-10">
       <h1 className="text-2xl font-semibold border-b-2 border-gray-300 py-4">
         Select checkbox from here
       </h1>
       <div className="flex flex-col items-start mt-8">
-        <label className="text-xl">
-          <input className="m-2 h-7 w-7 outline-none" type="checkbox" />
-          Checkbox number
-        </label>
+        {isCheckBoxChecked.map((state: any, index: any) => (
+          <label key={index} className="text-xl">
+            <input
+              className=" m-2 w-4 h-4 md:h-7 md:w-7 outline-none"
+              type="checkbox"
+              checked={state.checked}
+              onChange={() => onCheckBoxChange(index)}
+            />
+            Checkbox number {index}
+          </label>
+        ))}
       </div>
     </div>
   );
